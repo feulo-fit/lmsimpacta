d3c43a9a17781f4ebd8ac66e7e695c7fec88f8ba
Unknown <emilio.murta@msitbrasil.com>
2018-11-10 08:45:57 -0200

    + classes base para atividades

--- lms/models/__init__.py
+++ lms/models/__init__.py
@@ -1,2 +1,3 @@
 from .usuarios import Usuario, Aluno, Coordenador, Professor
 from .disciplinas import Curso, Disciplina, DisciplinaOfertada, SolicitacaoMatricula, Turma
+from .atividades import Atividade
--- lms/models/disciplinas.py
+++ lms/models/disciplinas.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-from .usuarios import Aluno, Coordenador, Professor
+from lms.models import Aluno, Coordenador, Professor
 
 class Disciplina(models.Model):
     nome = models.CharField(max_length=255, unique=True)
