d4cc28de51fdd3f65b06221f91889d0af2bd8ac7
Unknown <emilio.murta@msitbrasil.com>
2018-11-12 08:22:14 -0200

    + classes base para mensagens

---
+++ chat/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+from lms.models import Aluno, Professor
+
+# Create your models here.
+class Mensagem(models.Model):
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    professor = models.ForeignKey(Professor, on_delete=models.PROTECT)
+    assunto = models.CharField(max_length=255)
+    referencia = models.CharField(max_length=255)
+    conteudo = models.TextField(max_length=500)
+    status = models.CharField(max_length=100, default='Enviada', blank=True, null=True)
+    data_envio = models.DateField(default=timezone.now, blank=True, null=True, editable=False)
+    data_resposta = models.DateField(default=None, blank=True, null=True)
+    resposta = models.TextField(max_length=500, default=None, blank=True, null=True)
--- lmsimpacta/settings.py
+++ lmsimpacta/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lms.apps.LmsConfig',
+    'chat.apps.ChatConfig'
 ]
 
 MIDDLEWARE = [
