da6af0f3a3fa38461bbdba7fa64947be61b35f5e
Unknown <emilio.murta@msitbrasil.com>
2018-11-18 17:51:21 -0200

    + Vincula Usuario ao django.contrib.auth.models.User

--- lms/admin.py
+++ lms/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
-from lms.models.usuarios import Aluno, Coordenador, Professor
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from lms.models.usuarios import Usuario, Aluno, Coordenador, Professor
 from lms.models.disciplinas import Curso, Disciplina, DisciplinaOfertada, SolicitacaoMatricula, Turma
 from lms.models.atividades import Atividade, AtividadeVinculada, Entrega
 
+# Register admin behavior
+class UsuarioInline(admin.StackedInline):
+    model = Usuario
+    can_delete = False
+    verbose_name_plural = 'usuários'
+
+# Re-register UserAdmin
+class UserAdmin(BaseUserAdmin):
+    inlines = (UsuarioInline,)
+
 # Register your models here.
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
 admin.site.register(Aluno)
 admin.site.register(Professor)
 admin.site.register(Coordenador)
--- lms/models/usuarios.py
+++ lms/models/usuarios.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from datetime import date
 
 # Create your models here.
@@ -6,6 +7,7 @@ class Usuario(models.Model):
     login = models.CharField(max_length=100, unique=True)
     senha = models.CharField(max_length=100)
     dt_expiracao = models.DateField(default=date(year=1900, month=1, day=1), blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True, default=None)
 
 class Pessoa(models.Model):
     nome = models.CharField(max_length=255)
